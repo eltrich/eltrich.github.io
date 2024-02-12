d48de1c0816c66e27358e1e06fd3bf7e8ef3ee87
eltrich <156402153+eltrich@users.noreply.github.com>
2024-02-12 10:10:23 +0200

    Update main.js

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -1,6 +1,7 @@
 //selectors
 var tablinks=document.querySelectorAll('.tab-links');
 var tabcontents=document.querySelectorAll('.tab-contents');
+var pages=document.querySelectorAll('.pages');
 //functions
 function openTab(tabName){
 
@@ -17,5 +18,19 @@ tabcontent.classList.remove('active-tab');
 }
 event.currentTarget.classList.add('active-link');
 document.getElementById(tabName).classList.add('active-tab');
+
+}
+//pages- function
+function viewPage(){
+
+for(tablink of tablinks){
+
+tablink.classList.remove('active-page');
+
+}
+
+
+event.currentTarget.classList.add('active-page');
+
 
 }
